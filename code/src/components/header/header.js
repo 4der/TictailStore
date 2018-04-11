fd2c262243a6dfe11d8a0bbf75272b7aed6069d6
Anna Ågren <info@annaagren.se>
2018-04-11 14:24:40 +0200

    media queiries on heather

--- code/src/components/header/header.js
+++ code/src/components/header/header.js
@@ -2,6 +2,8 @@ import React from "react"
 import { BrowserRouter, Route, Link } from "react-router-dom"
 import "./header.css"
 import coolgal from "../../images/coolgal.jpg"
+import coolgalMobile from "../../images/coolgal-mobile.jpg"
+import coolgalTablet from "../../images/coolgal-tablet.jpg"
 import shoppingbag from "../../images/shoppingbag.jpg"
 
 class Header extends React.Component {
@@ -20,12 +22,24 @@ class Header extends React.Component {
       </div>
 
       <div className="hero-container">
-        <div className="hero-img">
-          <img src={coolgal} alt="hello!" />
+        <div className="hero-img-container">
+          <div className="hero-desktop-img">
+            <img src={coolgal} alt="hello!" />
+          </div>
+
+          <div className="hero-mobile-img">
+            <img src={coolgalMobile} alt="hello!" />
+          </div>
+
+          <div className="hero-tablet-img">
+            <img src={coolgalTablet} alt="hello!" />
+          </div>
         </div>
 
         <div className="categories-container">
+
           <div className="shop-headline">Look out <br /> for the news</div>
+
           <div className="category-txt-container">
             <div className="category-txt"><Link to="/categories/shoes">Shoes</Link></div>
             <div className="category-txt"><Link to="/categories/jewelry">Jewelry</Link></div>
